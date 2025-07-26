e93500e2dcdae1c772caaceb18ae7ec0d8babc41
Kozak-project <k_kozak@onet.pl>
2025-07-26 20:48:40 +0200

    First structure code of the chatbot

---
+++ main.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI
+from config import BOT_USERNAME, TOKEN
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+app = FastAPI()
+
+#Commands
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Hi, how can I help you (feel free to use any language you want)?')
+
+
+# Response
+
+def handle_response(text: str) -> str:
+    processed: str = text.lower()
+
+    if 'i need help' in processed:
+        return 'Ok let\'s begin'
+
+    return 'I don\'t understand what you wrote'
+
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+
+    print(f'User: {update.message.chat.id} in {message_type}: "{text}"')
+
+    if message_type is 'group':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+    else:
+        response: str = handle_response(new_text)
+
+    print(f'Bot: {response}')
+    await update.message.reply_text(response)
+
+
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update: {update}, caused error: {context.error}')
+
+
+if __name__ == '__main__':
+    print('Starting bot...')
+    app = Application.builder().token(TOKEN).build()
+
+    # Commands
+    app.add_handler(CommandHandler('start', start_command))
+
+    # Messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    # Errors
+    app.add_error_handler(error)
+
+    # Polls the bot
+    print('Polling...')
+    app.run_polling(poll_interval=3)
