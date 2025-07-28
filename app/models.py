d6f3e3bab1b2f88b96c82ca96bf4efa3f98fd8c7
Kozak-project <k_kozak@onet.pl>
2025-07-28 21:51:04 +0200

    Added User class model

--- app/models.py
+++ app/models.py
@@ -1,1 +1,22 @@
+from sqlalchemy import String, Column, Integer, Boolean
+from sqlalchemy.orm import relationship
 from database import Base
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(45), nullable=False)
+    last_name = Column(String(45), nullable=True)
+    username = Column(String(45), nullable=True)
+    is_bot = Column(Boolean, nullable=False)
+    language_code = Column(String(45), nullable=True)
+
+
+class Conversation(Base):
+    pass
+
+
+class Message(Base):
+    pass
